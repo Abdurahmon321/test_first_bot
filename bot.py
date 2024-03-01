280606c235d8f116700a6438903bc2841a7fa979
Abdurahmon <abdurahmonhoshimov2004@gmail.com>
2024-03-01 15:30:04 +0500

    start qo'shildi

--- bot.py
+++ bot.py
@@ -2,6 +2,10 @@ from telebot import TeleBot
 
 bot = TeleBot("6878848160:AAH6QkZs5ecP8gxg7Ym9IxZcpj3uUdVYpns")
 
+@bot.message_handler(commands=["start"])
+def start(messege):
+    chat_id = messege.chat.id
+    bot.send_message(chat_id, "salom")
 
 
 bot.polling()
